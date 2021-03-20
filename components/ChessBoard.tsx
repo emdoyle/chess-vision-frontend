c486c0cbf82cdb9d16664d395a885f0123c1e88c
Evan Doyle <evanmdoyle@gmail.com>
2021-03-20 15:49:57 -0700

    Use chessops for logic in chessground

--- components/ChessBoard.tsx
+++ components/ChessBoard.tsx
@@ -1,6 +1,26 @@
+import { Chess, parseSquare, SquareName } from "chessops";
+import { chessgroundDests } from "chessops/compat";
 import Chessground from "react-chessground";
 import "react-chessground/dist/styles/chessground.css";
+import { useState } from "react";
 
 export const ChessBoard = () => {
-  return <Chessground />;
+  const [chess, _] = useState<Chess>(Chess.default());
+  const [dests, setDests] = useState<Map<SquareName, SquareName[]>>(
+    chessgroundDests(chess)
+  );
+  return (
+    <Chessground
+      turnColor={chess.turn}
+      movable={{
+        free: false,
+        dests: dests,
+        color: chess.turn,
+      }}
+      onMove={(from, to) => {
+        chess.play({ from: parseSquare(from), to: parseSquare(to) });
+        setDests(chessgroundDests(chess));
+      }}
+    />
+  );
 };
