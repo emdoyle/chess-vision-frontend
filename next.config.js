bd527cfcddb22fadbe263aa0dcbc1e111eab00bf
Evan Doyle <evanmdoyle@gmail.com>
2021-11-06 20:59:18 -0400

    Upgrade next, add cross origin headers to type-to-move route

--- next.config.js
+++ next.config.js
@@ -14,6 +14,19 @@ module.exports = {
           },
         ],
       },
+      {
+        source: "/type-to-move",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
     ];
   },
 };
